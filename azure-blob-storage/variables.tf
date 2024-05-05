5d3311dc9ccbbe26c6d48b7333466162cdea75d4
Louisietta <127014554+Louisietta@users.noreply.github.com>
2024-05-05 14:55:31 +0200

    Update variables.tf 4

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,6 +1,12 @@
 variable "azurerm_resource_group"{
-  type = string
-  default  = value1
+  type = object({
+            name = string
+            location = string
+            })
+  default  = {
+            name = value1
+            location = value11
+            }
 
 variable "azurerm_storage_account"{
   type = object({
@@ -19,5 +25,5 @@ variable "azurerm_storage_container"{
 
 variable " azurerm_storage_blob"{
   type = string
-  default = value7
+  default = url
 }
