644b842a7e420c91f3a5a492f38de2beab42e1df
Louisietta <127014554+Louisietta@users.noreply.github.com>
2024-05-05 14:36:46 +0200

    Update main.tf 4

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,23 +1,25 @@
 terraform {
   backend "local" {
     path = "/.terraform.tfstate"
+  }
 }
-}
+
 resource "resource_group"{
   group = var.azurerm_resource_group
 }
-resource "resource_group"{
-  account = var.azurerm_storage_account
-}
+
 resource "storage_account"{
   account = var.azurerm_storage_account
 }
+
 resource "service_plan"{
   service = var.azurerm_service_plan
 }
+
 resource "linux_app"{
   linuxapp = var.azurerm_linux_function_app
 }
+
 resource "function_app"{
   functionapp = var.azurerm_function_app_function
 }
