98b602947b95954353e4e7c55e0e3416a028d64d
Louisietta <127014554+Louisietta@users.noreply.github.com>
2024-05-05 14:58:19 +0200

    Update main.tf 5

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -5,11 +5,13 @@ terraform {
 }
 
 resource "resource_group"{
-  group = var.azurerm_resource_group
+  groupn = var.azurerm_resource_group.name
+  groupl = var.azurem_resource_group.location
 }
 
 resource "storage_account"{
-  account = var.azurerm_storage_account
+  accountl = var.azurerm_storage_account.login
+  accountp = var.azurerm_storage_account.password
 }
 
 resource "service_plan"{
